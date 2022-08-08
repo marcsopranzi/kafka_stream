b575912d212f7dd49afd8ede65d0cea452d3a960
Marcelo <marcsopranzi@gmail.com>
2022-08-08 18:30:31 +0100

    writting back to cassandra

--- consumer_spark.py
+++ consumer_spark.py
@@ -1,52 +1,44 @@
-#  spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 consumer_spark.py
+#  spark-submit --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.0.0,com.datastax.spark:spark-cassandra-connector_2.12:3.0.0 consumer_spark.py
+# create keyspace logs_keyspace with replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+# use logs_keyspace;
+# create table logs_keyspace.logs_table (transaction_id text ,transaction_region text ,transaction_size int ,transaction_datetime text ,tran_date text ,id text primary key);
+
 from pyspark.sql import SparkSession, SQLContext
 from pyspark import SparkConf, SparkContext
+# import org.apache.spark.sql.cassandra._
 
 from pyspark.sql.types import *
 import pyspark.sql.functions as fn
 
+import uuid
 import config
 
 TOPIC_NAME = "test"
 KAFKA_BOOTSTRAP_SERVERS_URI = 'localhost:9092'
 
-# MongoDB Cluster Details
-mongodb_host_name = "kafkacluster.i1qem1x.mongodb.net"
-mongodb_port_no = "27017"
-mongodb_user_name = config.report_user['user_name']
-mongodb_password = config.report_user['user_password']
-mongodb_database_name = "kafkaCluster"
-mongodb_collection_name = "regions_traffic"
-mongo_uri = "mongodb://" + mongodb_user_name + ":" + mongodb_password + "@" + \
-    mongodb_host_name + ":" + mongodb_port_no
-spark_mongodb_output_uri = mongo_uri + "/" + \
-    mongodb_database_name + "." + mongodb_collection_name
+# Cassandra Cluster Details
+cassandra_connection_host = "localhost"
+cassandra_connection_port = "9042"
+cassandra_keyspace_name = "logs_keyspace"
+cassandra_table_name = "logs_table"
 
 
-# def save_to_mongodb_collection(current_df, epoc_id, mongodb_collection_name):
-#     print("Inside save_to_mongodb_collection function")
-#     print("Printing epoc_id: ")
-#     print(epoc_id)
-#     print("Printing mongodb_collection_name: " + mongodb_collection_name)
+def save_to_cassandra_table(current_df, epoc_id):
+    print("Inside save_to_cassandra_table function")
+    print("Printing epoc_id: ")
+    print(epoc_id)
 
-#     current_df.write.format("mongo") \
-#         .mode("append") \
-#         .option("uri", spark_mongodb_output_uri) \
-#         .option("database", mongodb_database_name) \
-#         .option("collection", mongodb_collection_name) \
-#         .save()
-#     print("Exit out of save_to_mongodb_collection function")
+    current_df \
+    .write \
+    .format("org.apache.spark.sql.cassandra") \
+    .mode("append") \
+    .option("spark.cassandra.connection.host", cassandra_connection_host) \
+    .option("spark.cassandra.connection.port", cassandra_connection_port) \
+    .option("keyspace", cassandra_keyspace_name) \
+    .option("table", cassandra_table_name) \
+    .save()
+    print("Exit out of save_to_cassandra_table function")
 
-
-# def write_mongo_row(df, epoch_id):
-#     mongoURL = spark_mongodb_output_uri
-#     df.write.format("mongo").\
-#         mode("append").\
-#         option("uri", mongoURL).save()
-
-def write_row(batch_df , batch_id):
-    batch_df.write.format("mongo").mode("append").save()
-    pass
 
 if __name__ == "__main__":
     print("Ingestion Started...")
@@ -55,8 +47,6 @@ if __name__ == "__main__":
         .builder \
         .appName("Live Stream Data") \
         .config("spark.sql.debug.maxToStringFields", "100") \
-        .config("spark.mongodb.output.uri", mongo_uri) \
-        .config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.12:3.0.1') \
         .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
@@ -81,40 +71,42 @@ if __name__ == "__main__":
     logs_df_2 = logs_df_1. \
         select(fn.from_json(fn.col("value"), schema).
                alias("logs_json"))
+    
+    logs_df_3 = logs_df_2.select('logs_json.*')
 
-    logs_df_3 = logs_df_2.select(
-        "logs_json.*").withColumn("timestamp", fn.current_timestamp())
+    uuidUdf= fn.udf(lambda : str(uuid.uuid4()),StringType()) 
 
-    logs_stream_df = logs_df_3 \
+    logs_df_4 = logs_df_3.withColumn("tran_date", fn.from_unixtime(fn.col("transaction_datetime"), "yyyy-MM-dd HH:mm:ss")).withColumn("id",uuidUdf())
+
+    logs_stream_df = logs_df_4 \
         .writeStream \
         .trigger(processingTime='5 seconds') \
         .outputMode("update") \
         .option("truncate", "false")\
         .format("console") \
-        .start()
+        .start()  
 
-    region_df_agg = logs_df_3.groupby('transaction_region').agg(fn.sum('transaction_size').
-                                                                alias('transaction_size'), fn.max('timestamp').alias('timestamp'))
+    logs_df_4.writeStream \
+      .trigger(processingTime='5 seconds') \
+      .format("json") \
+      .option("path", "data/json/trans_detail_raw_data") \
+      .option("checkpointLocation", "data/checkpoint/trans_detail_raw_data") \
+      .start()
+    
+    logs_df_4.printSchema()
 
-    region_df_agg = region_df_agg \
-        .writeStream \
+    print('\nlogs_df_4\n',type(logs_df_4))
+
+    logs_df_4.writeStream \
         .trigger(processingTime='5 seconds') \
         .outputMode("update") \
-        .option("truncate", "false")\
-        .format("console") \
-        .start()
+        .foreachBatch(save_to_cassandra_table) \
+        .start()    
 
-    region_df_agg.writeStream.foreachBatch(write_row).start().awaitTermination()
-
-# try:
-
-#     print('-----------------------------------------------------SAVING DF')
-#     region_df_agg.select('transaction_region', 'transaction_size', fn.timestamp_seconds().alias('time')).withWatermark('time', '10 minutes').\
-#         writeStream.foreachBatch(write_mongo_row).start()
-#     print('----------------------------------------------------DF SAVED')
-# except Exception as e:
-#     print('--------------------------------------------failure')
-#     print(e)
-#     print('---------------------------------------------error finished')
-
-logs_stream_df.awaitTermination()
+    # logs_df_4.write.cassandraFormat(cassandra_keyspace_name, cassandra_table_name)\
+    #     .mode("append")\
+    #     .save()
+   
+    
+    
+    logs_stream_df.awaitTermination()
