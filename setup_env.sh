b50141b1888e23c8c0bff7d95a30b62c166c80e4
marcelo sopranzi <marcsopranzi@gmail.com>
2023-05-20 14:51:49 +0100

    introduced pipenv

--- setup_env.sh
+++ setup_env.sh
@@ -6,14 +6,9 @@ sudo apt install python3.10-venv
 echo "Installing python pip"
 sudo apt install python3-pip
 
-echo "Installing venv"
-python3 -m venv venv_kafka_stream
+pip install pipenv
 
-echo "Activating venv"
-source ./venv_kafka_stream/bin/activate
+pipenv shell
 
 echo "Install dependencies"
-pip install numpy kafka-python
-
-echo "Pip upgrade"
-ppython3 -m pip install --upgrade pip
+pipenv install numpy kafka-python
