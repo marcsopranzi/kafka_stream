de732ac25e139a422e65d171d98f0bfbfe690603
marcelo sopranzi <marcsopranzi@gmail.com>
2023-07-05 15:48:46 +0100

    update setup file

--- setup_env.sh
+++ setup_env.sh
@@ -5,13 +5,9 @@ sudo apt install python3-pip
 
 pip install pipenv
 
-mkdir .env
-
 pipenv shell
 
 echo "Install dependencies"
 pipenv install numpy kafka-python pytest mock pre-commit
 
 pre-commit install
-
-sudo docker-compose up
